d8bbd38c25d335e2265ba2f9398c6389d0b4fae8
JohnWang512 <hejianjohn512@gmail.com>
2018-01-27 14:32:34 -0800

    I added javascript

---
+++ myscripts.js
@@ -0,0 +1,20 @@
+var x =("my message look at it");
+alert(x)
+
+function myFunction(p1,p2) {
+return p1 * p2;
+ }
+
+function myOtherFunction(p1,p2) {
+return p1 + p2
+}
+
+alert(myFunction(6,7));
+alert(myOtherFunction(1,3));
+
+console.log(x)
+
+function changeHeader(){
+heading = document.querySelector("h1");
+heading.innerHTML = "My new awsome message" 
+}
