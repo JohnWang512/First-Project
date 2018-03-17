5110efe8940373f124302a0a058e7e016fe5c7bb
JohnWang512 <hejianjohn512@gmail.com>
2018-03-17 15:06:49 -0700

    I added grids and jquery

--- myscripts.js
+++ myscripts.js
@@ -1,22 +1,22 @@
 
-1
-2
-3
 $( document ).ready(function() {
     
-    
-    
-console.log("jquery is working");
-  // Handler for .ready() called.
-});
-
-
+    var txt1 = "<button>New super button</button>";
+    console.log("jquery is working");
+    $("body").append(txt1);
+    $("body").click(function() {
+    $("body").append(txt1);
+    console.log("hello");
+    $("icon").toggleClass("ion-arrow-up-c");
+    });
+    // Handler for .ready() called.
+    });
 
 var x =("my message look at it");
 var pi = 3.14;
 var theMessage = "My new awsome message";
 
-//alert(x);
+alert(x);
 
 function myFunction(p1, p2) {
 return p1 * p2;
@@ -26,12 +26,24 @@ function myOtherFunction(p1, p2) {
 return p1 + p2;
 }
 
-//alert(myFunction(6,7));
-//alert(myOtherFunction(1,3));
+alert(myFunction(6,7));
+alert(myOtherFunction(1,3));
 
 console.log(x);
 
 if (pi<7) {
 theMessage = "My new cool message";
+}
+
+function changeHeader(){
+heading = document.querySelector("h1");
+document.querySelector("h1").innerHTML = theMessage; 
+}
+
+function myFunction(){
+var btn = document.createElement("BUTTON");       
+var t = document.createTextNode("CLICK ME");     
+btn.appendChild(t);                               
+document.body.appendChild(btn);                   
 }
 
